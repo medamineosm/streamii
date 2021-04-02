1f844e771782197302b0d18ce1f0f58dee58cdf3
Mohammed Amine OUASMINE <m.amine.osm@gmail.com>
2021-04-02 19:40:37 +0200

    Initial commit

---
+++ src/main/java/com/osm/streamii/app/StreamiiApplication.java
@@ -0,0 +1,13 @@
+package com.osm.streamii.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osm/streamii/app/StreamiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osm.streamii.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
